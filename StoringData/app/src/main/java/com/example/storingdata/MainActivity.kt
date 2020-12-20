b9386ee323036cacade3e7ece75aac69492e6966
Kaan Enes KAPICI <kaaneneskpc1@gmail.com>
2020-12-20 15:54:34 +0300

    BasicApps

--- StoringData/app/src/main/java/com/example/storingdata/MainActivity.kt
+++ StoringData/app/src/main/java/com/example/storingdata/MainActivity.kt
@@ -3,37 +3,39 @@ package com.example.storingdata
 import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import com.example.storingdata.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var sharedPreferences : SharedPreferences
     var preferences : String ?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         //SharedPreferences initialize
         sharedPreferences = this.getSharedPreferences("com.example.storingdata",MODE_PRIVATE)
         preferences = sharedPreferences.getString("data","0")
         if(preferences=="0"){
-            textView.text = "Your Data:"
+            binding.textView.text = "Your Data:"
         }
         else{
-            textView.text = "Your Data : $preferences"
+            binding.textView.text = "Your Data : $preferences"
         }
-        buttonSave.setOnClickListener {
+        binding.buttonSave.setOnClickListener {
 
-            val myData=editText.text.toString()
+            val myData=binding.editText.text.toString()
             if(myData!=null){
                 sharedPreferences.edit().putString("data",myData).apply()
-                textView.text = "Your Data : " + myData
+                binding.textView.text = "Your Data : " + myData
             }
 
         }
-        buttonDelete.setOnClickListener {
+        binding.buttonDelete.setOnClickListener {
             preferences = sharedPreferences.getString("data","0")
             if(preferences !="0"){
                 sharedPreferences.edit().remove("data").apply()
-                textView.text="Your Data : "
+                binding.textView.text="Your Data : "
             }
         }
     }
